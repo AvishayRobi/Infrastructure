773f5b49398d0ed1aae3bb0098fa7b3c4641c42b
AvishayRobi <79645481+AvishayRobi@users.noreply.github.com>
2022-07-10 14:17:33 +0300

    Update IntExtentionMethods.cs

--- Extensions/IntExtentionMethods.cs
+++ Extensions/IntExtentionMethods.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace WallaShops.Utils
 {
-  public static class ExtensionMethods
+  public static class IntExtensionMethods
   {
     public static void Times(this int count, Action action)
     {
