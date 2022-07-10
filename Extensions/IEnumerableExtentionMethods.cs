0b038eafc5be6f7a63941ed8e5acc2eaa1207756
AvishayRobi <79645481+AvishayRobi@users.noreply.github.com>
2022-07-10 14:15:23 +0300

    Create IEnumerableExtentionMethods.cs

---
+++ Extensions/IEnumerableExtentionMethods.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+namespace WallaShops.Utils
+{
+  public static class ExtensionMethods
+  {
+    public static void ApplyEach<T>(this IEnumerable<T> enumeration, Action<T> action)
+    {
+      foreach (T item in enumeration)
+      {
+        action(item);
+      }
+    }
+    
+    public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source)
+    {
+      return new HashSet<T>(source);
+    }
+  }
+}
